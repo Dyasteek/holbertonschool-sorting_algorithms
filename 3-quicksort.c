83935354cbf2b8e06d9ca3f7f3c501dd3bea7281
juandi131 <juandigarcia3@gmail.com>
2023-09-09 15:06:50 -0700

    12

--- 3-quicksort.c
+++ 3-quicksort.c
@@ -1,27 +1,70 @@
 #include "sort.h"
 /**
- * quick sort - sorts a rray of integers in ascending order
- *
- * @array: array of integers
+ * partition - Lomuto partition scheme
+ * @array: array to be sorted
+ * @low: starting index
+ * @high: ending index
  * @size: size of the array
- * Return: Void
+ *
+ * Return: index of the pivot
  */
+int partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1;
+	int j, temp;
 
-//void quick_sort(int *array, size_t size);
-//{
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+	if (array[i + 1] != array[high])
+	{
+		temp = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = temp;
+		print_array(array, size);
+	}
 
-//}
-int main() {
-    int arr[] = {1, 8, 3, 4, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
+	return (i + 1);
+}
 
-    printf("Array de enteros:\n");
-    
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    
-    printf("\n");
+/**
+ * quick_sort_helper - recursive function to perform quick sort
+ * @array: array to be sorted
+ * @low: starting index
+ * @high: ending index
+ * @size: size of the array
+ */
+void quick_sort_helper(int *array, int low, int high, size_t size)
+{
+	int pivot;
 
-    return 0;
+	if (low < high)
+	{
+		pivot = partition(array, low, high, size);
+		quick_sort_helper(array, low, pivot - 1, size);
+		quick_sort_helper(array, pivot + 1, high, size);
+	}
+}
+/**
+ * quick_sort - sorts an array of integers in ascending order using Quick sort
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+		quick_sort_helper(array, 0, size - 1, size);
 }
