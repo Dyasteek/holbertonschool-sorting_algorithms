74380bcc460509a09223e5771b5ff84655facb1b
lDyasteek <lawarsenik@gmail.com>
2023-09-10 21:16:55 +0000

    Aplied betty style

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -10,34 +10,34 @@
  */
 int lomuto_partition(int *array, int low, int high, size_t size)
 {
-    int pivot = array[high];
-    int i = low - 1;
-    int j, temp;
+	int pivot = array[high];
+	int i = low - 1;
+	int j, temp;
 
-    for (j = low; j <= high - 1; j++)
-    {
-        if (array[j] <= pivot)
-        {
-            i++;
-            if (i != j)
-            {
-                temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-                print_array(array, size);
-            }
-        }
-    }
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 
-    if (array[i + 1] != array[high])
-    {
-        temp = array[i + 1];
-        array[i + 1] = array[high];
-        array[high] = temp;
-        print_array(array, size);
-    }
+	if (array[i + 1] != array[high])
+	{
+		temp = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = temp;
+		print_array(array, size);
+	}
 
-    return (i + 1);
+	return (i + 1);
 }
 
 /**
@@ -49,12 +49,13 @@ int lomuto_partition(int *array, int low, int high, size_t size)
  */
 void quick_sort_helper(int *array, int low, int high, size_t size)
 {
-    if (low < high)
-    {
-        int pivot = lomuto_partition(array, low, high, size);
-        quick_sort_helper(array, low, pivot - 1, size);
-        quick_sort_helper(array, pivot + 1, high, size);
-    }
+	if (low < high)
+	{
+		int pivot = lomuto_partition(array, low, high, size);
+
+		quick_sort_helper(array, low, pivot - 1, size);
+		quick_sort_helper(array, pivot + 1, high, size);
+	}
 }
 
 /**
@@ -64,8 +65,8 @@ void quick_sort_helper(int *array, int low, int high, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    quick_sort_helper(array, 0, size - 1, size);
+	quick_sort_helper(array, 0, size - 1, size);
 }
